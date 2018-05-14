9679218450f5e87b31d6a6bbd95761e05a0c89c0
Sankar <sankar1535@gmail.com>
2018-05-14 18:30:21 +0200

    partial coherence

--- partial_coherence_fieldtrip.m
+++ partial_coherence_fieldtrip.m
@@ -1,35 +1,41 @@
 %% coherence between speech and all egg channels using diiferent metric value
 clc; clear; close all;
 
-
-data_path = 'C:\Users\SMukherjee\Desktop\projects\listen_italian_motor_entrainment\analysis\python\data\partialCoh\';
+data_path = '..\data\partialCoh\';
+% data_path = '\\10.96.7.1\projects\current\listen_italian_motor_entrainment\analysis\python\data\partialCoh\';
 
 trailLen = 2;
 removedFirst = 0;
 feature = {'envelop';'jawaopening';'lipaparature';'TTCD';'TBCD';'TMCD';'lipProtrusion'};
-condition = {'hyper','normal','hypo','all'};
-delay = 0;
+condition = {'Hyper','Normal','Hypo','All'};
+delay = 0:0.1:1;
 
 subject_name = {'Alice','Lucrezia','Elena','Jonluca','Manu','Sara','Marco','Elisa','Pasquale','Linda','Leonardo','Gianluca1','Federica','Silvia','Andrea','Giorgia','Laura','Daniel','Giada','Pagani','Silvia2',...
     'Elenora','Martina','Tommaso','Francesca'};
 
-remove_feature_contribution = {{'envelop'};{'jawaopening'};{'envelop'};{'lipaparature'};};
 freq_band = [1:40];
 
-data = [];
-data.Feature = [];
-data.RemovedFeature = [];
-data.Condition = [];
-data.Delay = [];
-data.Data = [];
-data.noTrials = [];
-data.Subject = [];
+for s = 1:length(subject_name)
+    k=1;
 
-k=1;
-for s = 1:length(subject_name)    
+    data = [];
+    data.Feature = [];
+    data.RemovedFeature = [];
+    data.Condition = [];
+    data.Delay = [];
+    data.Data = [];
+    data.noTrials = [];
+    data.Subject = [];
     for c=1:length(condition)
         for d=1:length(delay)
-            fiff_file = [data_path 'partialCoh-trailLen-' num2str(trailLen) '-removedFirst-' num2str(removedFirst) 's-condition-' condition{c} '-delay-' num2str(delay(d)) 's-' subject_name{s} '.fif'];
+            if(d==1)
+                dd = '0.0';
+            elseif(d==11)
+                dd = '1.0';
+            else
+                dd = num2str(delay(d));
+            end
+            fiff_file = [data_path 'partialCoh-trailLen-' num2str(trailLen) '-removedFirst-' num2str(removedFirst) 's-condition-' condition{c} '-delay-' dd 's-' subject_name{s} '.fif'];
             
             
             cfg = [];
@@ -71,12 +77,43 @@ for s = 1:length(subject_name)
             end
         end
     end
+    
+    freq=freqA.freq;
+    save([data_path 'PartialCoherence-' subject_name{s} '.mat'],'data','freq');
 end
-freq=freqA.freq;
+
+
+%% combine them all to single file
+data = [];
+data.Feature = [];
+data.RemovedFeature = [];
+data.Condition = [];
+data.Delay = [];
+data.Data = [];
+data.noTrials = [];
+data.Subject = [];
+
+for k = 1:length(subject_name)
+    a = load([data_path 'PartialCoherence-' subject_name{k} '.mat']);
+    
+    data.Feature{k} = a.data.Feature;
+    data.RemovedFeature{k} = a.data.RemovedFeature;
+    data.Condition{k} = a.data.Condition;
+    data.Delay{k} = a.data.Delay;
+    data.Data{k} = a.data.Data;
+    data.noTrials{k} = a.data.noTrials;
+    data.Subject{k} = a.data.Subject;
+end
+data1 = data;
+data = [];
+data.Feature = horzcat(data1.Feature{:});
+data.RemovedFeature = horzcat(data1.RemovedFeature{:});
+data.Condition = horzcat(data1.Condition{:});
+data.Delay = horzcat(data1.Delay{:});
+data.Data = horzcat(data1.Data{:});
+data.noTrials = horzcat(data1.noTrials{:});
+data.Subject = horzcat(data1.Subject{:});
 save([data_path 'PartialCoherence.mat'],'data','freq');
-
-
-
 
 
 
